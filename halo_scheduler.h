2f7b2682f0e3bcd23617bdd76a5db731d732e6a8
Philipp Moritz <pcmoritz@gmail.com>
2016-09-22 17:41:56 -0700

    update APIs

---
+++ halo_scheduler.h
@@ -0,0 +1,14 @@
+#ifndef HALO_SCHEDULER
+#define HALO_SCHEDULER
+
+/* Establish a connection to a new client. */
+void new_client_connection(local_scheduler_state *s, int listener_sock);
+
+/* schedule a task on a given worker. */
+void schedule_on_worker(local_scheduler_state *s, task_spec *task, int client_id);
+
+/* Handle new incoming task that was scheduled by the globl scheduler on
+ * this local scheduler. */
+void schedule_task(local_scheduler_state *s, task_spec *task)
+
+#endif
