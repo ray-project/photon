0de6c459234643c1f7674210b4e459eee8deab39
Robert Nishihara <robertnishihara@gmail.com>
2016-10-06 14:47:37 -0700

    Fix formatting.

--- lib/python/photon_extension.c
+++ lib/python/photon_extension.c
@@ -54,44 +54,44 @@ static PyMethodDef PyPhotonClient_methods[] = {
 };
 
 static PyTypeObject PyPhotonClientType = {
-    PyObject_HEAD_INIT(NULL) 0,  /* ob_size */
-    "photon.PhotonClient",                 /* tp_name */
+    PyObject_HEAD_INIT(NULL) 0,          /* ob_size */
+    "photon.PhotonClient",               /* tp_name */
     sizeof(PyPhotonClient),              /* tp_basicsize */
-    0,                           /* tp_itemsize */
+    0,                                   /* tp_itemsize */
     (destructor) PyPhotonClient_dealloc, /* tp_dealloc */
-    0,                           /* tp_print */
-    0,                           /* tp_getattr */
-    0,                           /* tp_setattr */
-    0,                           /* tp_compare */
-    0,                           /* tp_repr */
-    0,                           /* tp_as_number */
-    0,                           /* tp_as_sequence */
-    0,                           /* tp_as_mapping */
-    0,                           /* tp_hash */
-    0,                           /* tp_call */
-    0,                           /* tp_str */
-    0,                           /* tp_getattro */
-    0,                           /* tp_setattro */
-    0,                           /* tp_as_buffer */
-    Py_TPFLAGS_DEFAULT,          /* tp_flags */
-    "Photon client object",               /* tp_doc */
-    0,                           /* tp_traverse */
-    0,                           /* tp_clear */
-    0,                           /* tp_richcompare */
-    0,                           /* tp_weaklistoffset */
-    0,                           /* tp_iter */
-    0,                           /* tp_iternext */
+    0,                                   /* tp_print */
+    0,                                   /* tp_getattr */
+    0,                                   /* tp_setattr */
+    0,                                   /* tp_compare */
+    0,                                   /* tp_repr */
+    0,                                   /* tp_as_number */
+    0,                                   /* tp_as_sequence */
+    0,                                   /* tp_as_mapping */
+    0,                                   /* tp_hash */
+    0,                                   /* tp_call */
+    0,                                   /* tp_str */
+    0,                                   /* tp_getattro */
+    0,                                   /* tp_setattro */
+    0,                                   /* tp_as_buffer */
+    Py_TPFLAGS_DEFAULT,                  /* tp_flags */
+    "Photon client object",              /* tp_doc */
+    0,                                   /* tp_traverse */
+    0,                                   /* tp_clear */
+    0,                                   /* tp_richcompare */
+    0,                                   /* tp_weaklistoffset */
+    0,                                   /* tp_iter */
+    0,                                   /* tp_iternext */
     PyPhotonClient_methods,              /* tp_methods */
-    0,                           /* tp_members */
-    0,                           /* tp_getset */
-    0,                           /* tp_base */
-    0,                           /* tp_dict */
-    0,                           /* tp_descr_get */
-    0,                           /* tp_descr_set */
-    0,                           /* tp_dictoffset */
+    0,                                   /* tp_members */
+    0,                                   /* tp_getset */
+    0,                                   /* tp_base */
+    0,                                   /* tp_dict */
+    0,                                   /* tp_descr_get */
+    0,                                   /* tp_descr_set */
+    0,                                   /* tp_dictoffset */
     (initproc) PyPhotonClient_init,      /* tp_init */
-    0,                           /* tp_alloc */
-    PyType_GenericNew,           /* tp_new */
+    0,                                   /* tp_alloc */
+    PyType_GenericNew,                   /* tp_new */
 };
 
 static PyMethodDef photon_client_methods[] = {
